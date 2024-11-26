5ae3cc13add380da0c1d7fef40a2bd3c36e1617c
Marcin Załęczny <mzaleczny@gmail.com>
2024-11-26 07:50:03 +0100

    Poprawki zwiazane ze sciezkami zasobow w cmake.

--- BuildEditor.bat
+++ BuildEditor.bat
@@ -1,20 +1,17 @@
 @echo off
 
-SET SFML_DIR="f:\libs\SFML\SFML-2.6.2"
-SET FILE_TO_COMPILE="src\main.cpp"
-mkdir Build
+SET FILE_TO_COMPILE="src\Editor\Editor.cpp"
 
-cl /JMC /permissive- /GS /W3 /Zc:wchar_t -I%SFML_DIR%\include /ZI /Gm- /Od /sdl /Fd"vc142.pdb" /Zc:inline /fp:precise /D "_DEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /MDd /FC /EHsc /nologo /diagnostics:column /std:c++20 %FILE_TO_COMPILE% %SFML_DIR%\lib\sfml-system-d.lib %SFML_DIR%\lib\sfml-main-d.lib %SFML_DIR%\lib\sfml-graphics-d.lib %SFML_DIR%\lib\sfml-audio-d.lib %SFML_DIR%\lib\sfml-window-d.lib /DEBUG /INCREMENTAL
 
-copy *.exe Build\
-copy *.ilk Build\
-copy *.obj Build\
-copy *.idb Build\
-copy *.pdb Build\
-copy *.dll Build\
+WHERE /Q cl.exe
+IF %ERRORLEVEL% NEQ 0 (
+    ECHO Ten skrypt powinien byc uruchamiany z konsoli "x64 Native Tools Command Prompt for VS"
+    BREAK
+) ELSE (
+    cl /JMC /permissive- /GS /W3 /Zc:wchar_t -I.\dependencies\SFML\include -I.\src /ZI /Gm- /Od /sdl /Fd"vc142.pdb" /Zc:inline /fp:precise /D "_DEBUG" /D "_CONSOLE" /D "_UNICODE" /D "UNICODE" /errorReport:prompt /WX- /Zc:forScope /Gd /MDd /FC /EHsc /nologo /diagnostics:column /std:c++20 %FILE_TO_COMPILE% %SFML_DIR%\lib\sfml-system-d.lib %SFML_DIR%\lib\sfml-main-d.lib %SFML_DIR%\lib\sfml-graphics-d.lib %SFML_DIR%\lib\sfml-audio-d.lib %SFML_DIR%\lib\sfml-window-d.lib /DEBUG /INCREMENTAL
 
-del *.exe
-del *.ilk
-del *.obj
-del *.idb
-del *.pdb
+    del *.ilk
+    del *.obj
+    del *.idb
+    del *.pdb
+)
--- build.bat
+++ build.bat
@@ -5,19 +5,3 @@ cmake ..
 REM cmake --build . --config Debug --verbose
 REM cmake --build . --config Release --verbose
 cd ..
-
-mkdir Build\Debug
-
-mkdir Build\Debug\assets
-xcopy assets Build\Debug\assets /S /E
-
-mkdir Build\Debug\basic_locations
-xcopy basic_locations Build\Debug\basic_locations /S /E
-
-mkdir Build\Debug\dialogues
-xcopy dialogues Build\Debug\dialogues /S /E
-
-mkdir Build\Debug\world
-xcopy world Build\Debug\world /S /E
-
-copy *.dll Build\Debug\
