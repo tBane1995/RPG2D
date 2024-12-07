4e8891854e68645b28575706d854548fb76c809d
tBane1995 <bboguslawski1995@gmail.com>
2024-12-07 12:10:14 +0100

    Poprawiono pozycjonowanie Scrollbara uwzględniając przesunięcie kamery - Scrollbar jest teraz statyczny

--- src/Platform/Scrollbar.cpp
+++ src/Platform/Scrollbar.cpp
@@ -24,82 +24,89 @@ Scrollbar::Scrollbar(sf::Vector2f size, sf::Vector2f position, short min_value, 
     bar_color = sf::Color::Red;
     scroll_color = sf::Color::Blue;
 
+    sf::Vector2f p;
+    p.x += cam->position.x + position.x;
+    p.y += cam->position.y + position.y;
+
     // BAR RECT
     bar_top = sf::RectangleShape(sf::Vector2f(size.x, size.x));
-    bar_top.setPosition(position);
+    bar_top.setPosition(p);
     bar_top.setFillColor(bar_color);
 
     bar_center = sf::RectangleShape(sf::Vector2f(size.x, size.y-2.0f*size.x));
-    bar_center.setPosition(position.x, position.y + size.x);
+    bar_center.setPosition(p.x, p.y + size.x);
     bar_center.setFillColor(bar_color);
 
     bar_bottom = sf::RectangleShape(sf::Vector2f(size.x, size.x));
-    bar_bottom.setPosition(position.x, position.y + size.y - size.x);
+    bar_bottom.setPosition(p.x, p.y + size.y - size.x);
     bar_bottom.setFillColor(bar_color);
    
     // SCROLL RECT
     scroll_top = sf::RectangleShape(sf::Vector2f(size.x,size.x));
-    scroll_top.setPosition(position.x, position.y+size.x);
+    scroll_top.setPosition(p.x, p.y+size.x);
     scroll_top.setFillColor(scroll_color);
 
     scroll_center = sf::RectangleShape(sf::Vector2f(size.x, getScrollSizeY()-2*size.x));
-    scroll_center.setPosition(position.x, position.y+size.x+size.x);
+    scroll_center.setPosition(p.x, p.y+size.x+size.x);
     scroll_center.setFillColor(sf::Color::Green);
 
     scroll_bottom = sf::RectangleShape(sf::Vector2f(size.x, size.x));
-    scroll_bottom.setPosition(position.x, position.y+size.x+size.x+getScrollSizeY()-2*size.x);
+    scroll_bottom.setPosition(p.x, p.y+size.x+size.x+getScrollSizeY()-2*size.x);
     scroll_bottom.setFillColor(scroll_color);
 
     // BAR SPRITES
     spr_bar_top = sf::Sprite(*getSingleTexture("GUI/scrollbar/bar_top")->texture);
     spr_bar_top.setScale(size.x/16.0f, size.x/16.0f);
-    spr_bar_top.setPosition(position);
+    spr_bar_top.setPosition(p);
 
     spr_bar_center = sf::Sprite(*getSingleTexture("GUI/scrollbar/bar_center")->texture);
     spr_bar_center.setScale(size.x/16.0f, (size.y-2.0f*size.x)/16.0f);
-    spr_bar_center.setPosition(position.x, position.y + size.x);
+    spr_bar_center.setPosition(p.x, p.y + size.x);
 
     spr_bar_bottom = sf::Sprite(*getSingleTexture("GUI/scrollbar/bar_bottom")->texture);
     spr_bar_bottom.setScale(size.x/16.0f, size.x/16.0f);
-    spr_bar_bottom.setPosition(position.x, position.y + size.y - size.x);
+    spr_bar_bottom.setPosition(p.x, p.y + size.y - size.x);
 
     // SCROLL SPRITES
     spr_scroll_top = sf::Sprite(*getSingleTexture("GUI/scrollbar/scroll_top")->texture);
     spr_scroll_top.setScale(size.x/16.0f, size.x/16.0f);
-    spr_scroll_top.setPosition(position.x, position.y+size.x);
+    spr_scroll_top.setPosition(p.x, p.y+size.x);
 
     spr_scroll_center = sf::Sprite(*getSingleTexture("GUI/scrollbar/scroll_center")->texture);
     spr_scroll_center.setScale(size.x/16.0f, (getScrollSizeY()-2*size.x)/16.0f);
-    spr_scroll_center.setPosition(position.x, position.y+size.x+size.x);
+    spr_scroll_center.setPosition(p.x, p.y+size.x+size.x);
 
     spr_scroll_bottom = sf::Sprite(*getSingleTexture("GUI/scrollbar/scroll_bottom")->texture);
     spr_scroll_bottom.setScale(size.x/16.0f, size.x/16.0f);
-    spr_scroll_bottom.setPosition(position.x, position.y + size.x + size.x + getScrollSizeY()-2*size.x);
+    spr_scroll_bottom.setPosition(p.x, p.y + size.x + size.x + getScrollSizeY()-2*size.x);
 
 }
 
 void Scrollbar::setPosition(sf::Vector2f pos) {
     this->position = pos;
 
+    sf::Vector2f p;
+    p.x += cam->position.x + position.x;
+    p.y += cam->position.y + position.y;
     // BAR RECT
     bar_top.setPosition(position);
-    bar_center.setPosition(position.x, position.y + size.x);
-    bar_bottom.setPosition(position.x, position.y + size.y - size.x);
+    bar_center.setPosition(p.x, p.y + size.x);
+    bar_bottom.setPosition(p.x, p.y + size.y - size.x);
 
     // BAR SPRITES
-    spr_bar_top.setPosition(position);
-    spr_bar_center.setPosition(position.x, position.y + size.x);
-    spr_bar_bottom.setPosition(position.x, position.y + size.y - size.x);
+    spr_bar_top.setPosition(p);
+    spr_bar_center.setPosition(p.x, p.y + size.x);
+    spr_bar_bottom.setPosition(p.x, p.y + size.y - size.x);
 
     // SCROLL RECT
-    scroll_top.setPosition(position.x, position.y+size.x);
-    scroll_center.setPosition(position.x, position.y + size.x+size.x);
-    scroll_bottom.setPosition(position.x, position.y + size.x + size.x + getScrollSizeY() - 2 * size.x);
+    scroll_top.setPosition(p.x, p.y+size.x);
+    scroll_center.setPosition(p.x, p.y + size.x+size.x);
+    scroll_bottom.setPosition(p.x, p.y + size.x + size.x + getScrollSizeY() - 2 * size.x);
 
     // SCROLL SPRITES
-    spr_scroll_top.setPosition(position.x, position.y+size.x);
-    spr_scroll_center.setPosition(position.x, position.y + size.x + size.x);
-    spr_scroll_bottom.setPosition(position.x, position.y + size.x + size.x + getScrollSizeY() - 2 * size.x);
+    spr_scroll_top.setPosition(p.x, p.y+size.x);
+    spr_scroll_center.setPosition(p.x, p.y + size.x + size.x);
+    spr_scroll_bottom.setPosition(p.x, p.y + size.x + size.x + getScrollSizeY() - 2 * size.x);
 }
 
 void Scrollbar::setValue(short value) {
@@ -122,15 +129,19 @@ void Scrollbar::scrollPositioning() {
 
     float delta_y = float(scroll_value) * ((size.y-2*size.x)/float(max_value-min_value+1));
 
+    sf::Vector2f p;
+    p.x += cam->position.x + position.x;
+    p.y += cam->position.y + position.y;
+
     // SCROLL RECT
-    scroll_top.setPosition(position.x, position.y + size.x + delta_y);
-    scroll_center.setPosition(position.x, position.y + size.x + size.x + delta_y);
-    scroll_bottom.setPosition(position.x, position.y + size.x + size.x + getScrollSizeY() - 2 * size.x + delta_y);
+    scroll_top.setPosition(p.x, p.y + size.x + delta_y);
+    scroll_center.setPosition(p.x, p.y + size.x + size.x + delta_y);
+    scroll_bottom.setPosition(p.x, p.y + size.x + size.x + getScrollSizeY() - 2 * size.x + delta_y);
 
     // SCROLL SPRITES
-    spr_scroll_top.setPosition(position.x, position.y + size.x + delta_y);
-    spr_scroll_center.setPosition(position.x, position.y + size.x + size.x + delta_y);
-    spr_scroll_bottom.setPosition(position.x, position.y + size.x + size.x + getScrollSizeY() - 2 * size.x + delta_y);
+    spr_scroll_top.setPosition(p.x, p.y + size.x + delta_y);
+    spr_scroll_center.setPosition(p.x, p.y + size.x + size.x + delta_y);
+    spr_scroll_bottom.setPosition(p.x, p.y + size.x + size.x + getScrollSizeY() - 2 * size.x + delta_y);
 }
 
 bool Scrollbar::isSelected() {
@@ -168,13 +179,17 @@ void Scrollbar::update(sf::Event& event) {
     else if (is_pressed) {
         float scrollPos = worldMousePosition.y - start_mouse_y;
 
-        if (scrollPos < position.y)
-            scrollPos = position.y;
+        sf::Vector2f p;
+        p.x += cam->position.x + position.x;
+        p.y += cam->position.y + position.y;
 
-        if (scrollPos > position.y + size.y - getScrollSizeY())
-            scrollPos = position.y + size.y - getScrollSizeY();
+        if (scrollPos < p.y)
+            scrollPos = p.y;
 
-        setValue((scrollPos-position.y) * (max_value - min_value + 1) / size.y);
+        if (scrollPos > p.y + size.y - getScrollSizeY())
+            scrollPos = p.y + size.y - getScrollSizeY();
+
+        setValue((scrollPos-p.y) * (max_value - min_value + 1) / size.y);
         scrollPositioning();
     }
     
