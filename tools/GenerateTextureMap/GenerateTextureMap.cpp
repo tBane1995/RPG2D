f9f919a5ef126c1338af914e01f1ea9eef19c668
Marcin Załęczny <mzaleczny@gmail.com>
2024-12-07 11:25:42 +0100

    Rozbudowa programu do generowania map tekstur, tak zeby generowal mniejsze mapy (2048x2048). Bo jedna duza ladowala sie dluzej niz ladowanie pojedynczych tekstur osobno.

--- tools/GenerateTextureMap/GenerateTextureMap.cpp
+++ tools/GenerateTextureMap/GenerateTextureMap.cpp
@@ -1,3 +1,6 @@
+#include <string>
+#include <vector>
+#include <map>
 #include <iostream>
 #include <fstream>
 #include <filesystem>
@@ -6,8 +9,8 @@
 #include <SFML/Graphics.hpp>
 #include <windows.h>
 
-constexpr int MapWidth = 8192;
-constexpr int MapHeight = 4096;
+constexpr int MapWidth = 2048;
+constexpr int MapHeight = 2048;
 
 struct TEntry
 {
@@ -17,20 +20,31 @@ struct TEntry
 };
 sf::Image ImageMap;
 std::vector<TEntry> Assets;
-std::string TextureImageFileName;
+std::map<std::string, std::vector<TEntry>> Maps;
+std::string TextureImageFileNameBase;
+std::string CurrentMapType;
 
 class TFile // obiekt typu RAII -> Rescource Aquisition Is Initialization
 {
 public:
-    TFile()
+    TFile() {}
+    ~TFile()
     {
+        Close();
+    }
+    void Open(std::string TextureImageFileName = "TextureMap.png")
+    {
+        Close();
         std::lock_guard<std::mutex> lock(mu);
         file.open(TextureImageFileName + ".txt", std::ios::out | std::ios::trunc);
     }
-    ~TFile()
+    void Close()
     {
         std::lock_guard<std::mutex> lock(mu);
-        file.close();
+        if (file.is_open())
+        {
+            file.close();
+        }
     }
     std::ofstream& operator<<(std::string Str)
     {
@@ -43,31 +57,67 @@ private:
 };
 
 
+void CreateMainTextureMaps(const std::string& Directory);
 void ProcessDirectory(const std::string& Directory, const std::string& Indent);
 void DrawOnImage(const sf::Image& Src, sf::Image& Dest, int DestX, int DestY);
-void JoinAssets();
+void JoinAssets(std::string CurrentMapType);
 
 int main(int argc, char** argv[])
 {
     std::string Directory("assets");
-    std::string Indent;
-    TextureImageFileName = Directory + "/TextureMap.png";
 
     std::cout << "Executing GenerateTextureMap, current working directory is: " << std::filesystem::current_path() << std::endl;
 
-    ImageMap.create(MapWidth, MapHeight, sf::Color(255, 255, 255, 0));
-    if (std::filesystem::exists(Directory + "/TextureMap.png"))
-    {
-        DeleteFile((Directory + "/TextureMap.png").c_str());
-    }
-    ProcessDirectory(Directory, Indent);
+    CreateMainTextureMaps(Directory);
 
-    JoinAssets();
-    ImageMap.saveToFile(TextureImageFileName);
+    TextureImageFileNameBase = "TextureMap";
+    for (auto it = Maps.begin(); it != Maps.end(); ++it)
+    {
+        JoinAssets((*it).first);
+    }
 
     return 0;
 }
 
+
+void CreateMainTextureMaps(const std::string& Directory)
+{
+    std::vector<std::string> MainDirectories;
+    MainDirectories.clear();
+    for (const auto& entry : std::filesystem::directory_iterator(Directory))
+    {
+        if (entry.is_directory())
+        {
+            if (entry.path().string() != Directory + "\\monsters" && entry.path().string() != Directory + "\\sets")
+            {
+                MainDirectories.push_back(entry.path().string());
+            }
+        }
+    }
+
+    // inicjalizacja map
+    Maps["Main"] = Assets;
+    Maps["Monsters"] = Assets;
+    Maps["Character"] = Assets;
+
+    std::string Indent;
+    // Glowna mapa
+    CurrentMapType = "Main";
+    for (size_t i = 0; i < MainDirectories.size(); ++i)
+    {
+        ProcessDirectory(MainDirectories[i], Indent);
+    }
+
+    // Mapa potworow
+    Indent = "";
+    CurrentMapType = "Monsters";
+    ProcessDirectory(Directory + "\\monsters", Indent);
+
+    // Mapa bohatera
+    Indent = "";
+    CurrentMapType = "Character";
+    ProcessDirectory(Directory + "\\sets", Indent);
+}
 
 void ProcessDirectory(const std::string& Directory, const std::string& Indent)
 {
@@ -103,7 +153,7 @@ void ProcessDirectory(const std::string& Directory, const std::string& Indent)
                 Item.Path = entry.path().string();
                 Item.Width = Im.getSize().x;
                 Item.Height = Im.getSize().y;
-                Assets.push_back(Item);
+                Maps[CurrentMapType].push_back(Item);
             }
         }
     }
@@ -129,13 +179,25 @@ void DrawOnImage(const sf::Image& Src, sf::Image& Dest, int DestX, int DestY)
     }
 }
 
-void JoinAssets()
+void JoinAssets(std::string CurrentMapType)
 {
-    TFile DataFile;
-    sf::Vector2u MapSize = ImageMap.getSize();
+    sf::Vector2u MapSize;
     int DestX = 0;
     int DestY = 0;
     int MaxLineHeight = 0;
+    int Counter = 0;
+    std::vector<TEntry>& Assets = Maps[CurrentMapType];
+    TFile DataFile;
+
+    std::string Fname = TextureImageFileNameBase + CurrentMapType;
+    if (Counter > 0)
+    {
+        Fname += std::to_string(Counter);
+    }
+    DataFile.Open("assets\\" + Fname + ".txt");
+
+    ImageMap.create(MapWidth, MapHeight, sf::Color(255, 255, 255, 0));
+    MapSize = ImageMap.getSize();
     std::sort(Assets.begin(), Assets.end(), [](const TEntry& a, const TEntry& b) { return a.Width < b.Width; });
     for (size_t i = 0; i < Assets.size(); ++i)
     {
@@ -144,6 +206,37 @@ void JoinAssets()
             DestY += MaxLineHeight;
             MaxLineHeight = 0;
             DestX = 0;
+            int NewLineMaxHeight = 0;
+            for (size_t j = i + 1; j < Assets.size(); ++j)
+            {
+                int x = DestX;
+                if (x + Assets[j].Width >= MapSize.x)
+                {
+                    break;
+                }
+                if (NewLineMaxHeight < Assets[j].Height)
+                {
+                    NewLineMaxHeight = Assets[j].Height;
+                }
+                x += Assets[j].Width;
+            }
+            if (DestY + NewLineMaxHeight >= MapSize.y)
+            {
+                Fname = TextureImageFileNameBase + CurrentMapType;
+                if (Counter > 0)
+                {
+                    Fname += std::to_string(Counter);
+                }
+                ImageMap.saveToFile("assets\\" + Fname + ".png");
+                DataFile.Close();
+
+                Counter++;
+                Fname = TextureImageFileNameBase + CurrentMapType + std::to_string(Counter);
+                DataFile.Open("assets\\" + Fname);
+                ImageMap.create(MapWidth, MapHeight, sf::Color(255, 255, 255, 0));
+                MapSize = ImageMap.getSize();
+                DestY = 0;
+            }
         }
         //std::cout << Assets[i].Path << ": " << Assets[i].Width << std::endl;
         if (DestX + Assets[i].Width < MapSize.x)
@@ -168,4 +261,5 @@ void JoinAssets()
             }
         }
     }
+    ImageMap.saveToFile("assets\\" + Fname + ".png");
 }
