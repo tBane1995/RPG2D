1384bad3e954c1678a243959144bc85dd2aaa869
tBane1995 <bboguslawski1995@gmail.com>
2024-12-08 11:58:16 +0100

    Poprawiono ostateczne pozycjonowanie ScrollableText

--- src/Platform/ScrollableText.cpp
+++ src/Platform/ScrollableText.cpp
@@ -21,13 +21,13 @@ ScrollableText::ScrollableText(std::wstring text) : Dialog(DialogType::Scrollabl
 	textarea->setPosition(sf::Vector2f(- width/2.0f + borderWidth + margin, - height/2.0f + borderWidth + margin));
 
 	sf::Vector2f scrollbar_position = sf::Vector2f(width/2.0f-borderWidth-scrollbar_size.x, -height/2.0f + borderWidth);
-	float scroll_max = textarea->getSize().y + textarea->getLineHeight();
+	float scroll_max = textarea->getSize().y + 2*textarea->getLineHeight();
 	float scroll_len = height;
 	scrollbar = new Scrollbar(scrollbar_size, scrollbar_position, 0, scroll_max, 0, scroll_len);
 	
 	// TO-DO - generowanie tekstury przewijalnej dla tekstu
 	text_texture.create(width-2.0f*(borderWidth+margin) - scrollbar_size.x, height-2.0f*(borderWidth+margin));
-	sf::View v = sf::View(sf::FloatRect(sf::Vector2f(cam->position.x-width/2.0f+borderWidth+margin, cam->position.y-height/2.0f+borderWidth+margin), sf::Vector2f(width-2*(borderWidth+margin), height-2*(borderWidth+margin))));
+	sf::View v = sf::View(sf::FloatRect(sf::Vector2f(cam->position.x-width/2.0f+borderWidth+margin, cam->position.y-height/2.0f+borderWidth+margin), sf::Vector2f(width-2*(borderWidth+margin)-scrollbar->size.x, height-2*(borderWidth+margin))));
 	text_texture.setView(v);
 	text_texture.setSmooth(true);
 	text_texture.clear(sf::Color::Transparent);
@@ -52,7 +52,7 @@ void ScrollableText::update(sf::Event& event) {
 	position.y = borderWidth + margin -scrollbar->scroll_value - height / 2.0f;
 	textarea->setPosition(position);
 
-	sf::View v = sf::View(sf::FloatRect(sf::Vector2f(cam->position.x - width / 2.0f + borderWidth + margin, cam->position.y - height / 2.0f + borderWidth + margin), sf::Vector2f(width - 2 * (borderWidth + margin), height - 2 * (borderWidth + margin))));
+	sf::View v = sf::View(sf::FloatRect(sf::Vector2f(cam->position.x - width / 2.0f + borderWidth + margin, cam->position.y - height / 2.0f + borderWidth + margin), sf::Vector2f(width - 2 * (borderWidth + margin)-scrollbar->size.x, height - 2 * (borderWidth + margin))));
 	text_texture.setView(v);
 	text_texture.clear(sf::Color::Transparent);
 	text_texture.draw(textarea->rect);
