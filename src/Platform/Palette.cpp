c5de38ef69e0f52f6d640dffe89b893905d4b6b4
Bartosz Bogusławski <bboguslawski1995@gmail.com>
2025-01-14 20:21:52 +0100

    Poprawienie pozycjonowanie Tipów (wskazówek)

--- src/Platform/Palette.cpp
+++ src/Platform/Palette.cpp
@@ -793,6 +793,8 @@ void Palette::loadPalette() {
                     sf::Vector2f pos;
                     pos.x = button->position.x - button->sprite.getTexture()->getSize().x / 2.0f;
                     pos.y = button->position.y + button->sprite.getTexture()->getSize().y / 4.0f;
+                    pos.x = button->position.x - button->sprite.getGlobalBounds().width / 2.0f;
+                    pos.y = button->position.y + button->sprite.getGlobalBounds().height / 2.0f;
                     if (tip != nullptr)
                         delete tip;
                     tip = new Tip(ConvertUtf8ToWide(button->object->name), pos, button);       // TO-DO delete convert and use std::wstring
